188dc3875f6df9bccf4079e539c34efd5f1e0dc4
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-26 22:29:23 -0500

    Create src.py

---
+++ bot/src.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from g4f.client import Client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv("GPT_TOKEN")
+bot = commands.Bot(command_prefix="-")
+client = Client()
+@bot.command()
+async def gpt(ctx, *, prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+    )
+    gpt_response = str(response.choices[0].message.content)
+    await ctx.send(str(gpt_response))
+bot.run(token)
